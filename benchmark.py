bec19f91f0c9d38add18f70d7a84d3a5530f9b80
hennadiy-aliiev <you@example.com>
2017-03-24 16:33:19 +0200

    update

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,5 @@
 from flask_bcrypt import generate_password_hash
 
-# Change the number of rounds (second argument) until it takes between
-# 0.25 and 0.5 seconds to run.
-generate_password_hash('password1', 12)
+
+generate_password_hash('password1', 10)
+
